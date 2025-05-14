21e18a172d3bacf289cae64703f6f0b23743dcc1
michii <xuzrIp-hiwcot-0wegty>
2025-05-14 21:37:23 +0200

    chore: update gitignore and remove node_modules from git

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
+}); 
